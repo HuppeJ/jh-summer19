6c954c1173191ca562f44cc0c7b5fc19ec3f3e20
HuppeJ <jeremie.huppe@gmail.com>
2019-06-20 12:39:30 -0400

    Added get_synonyms

--- project/rfut/app.py
+++ project/rfut/app.py
@@ -1,10 +1,13 @@
+import time 
+
 from rfut.scripts.find_questions_with_coreNLP import run as run_find_questions_with_coreNLP
 from rfut.scripts.get_percentage_of_threads_data import run as run_get_percentage_of_threads_data
 from rfut.scripts.parse_posts_to_sentences import run as run_parse_posts_to_sentences
 from rfut.scripts.remove_short_messages import run as run_remove_short_messages
-import time 
+from rfut.scripts.get_synonyms import run as run_get_synonyms
+
 def run():
     start_time = time.time()
     print("Project R.F.U.T. is running!")
-    run_remove_short_messages()
+    run_get_synonyms()
     print("--- %s seconds ---" % (time.time() - start_time))
---
+++ project/rfut/scripts/get_synonyms.py
@@ -0,0 +1,55 @@
+### Algorithm: 
+
+# - 1. Get the list of initial keywords retrieved manually through conversation analysis from annotation.csv
+# - 2. Split post_messageText into sentences
+# - 3. Clean sentences text
+
+# Imports
+import csv
+import os
+import re
+import pandas as pd
+from nltk.corpus import wordnet
+from rfut.common.constants import PROJECT_PATH, DATA_OUTPUT_PATH
+
+
+def run():
+    print("Running get_synonyms")
+    
+    # Load initial keywords list
+    threads_sample_path = [PROJECT_PATH, DATA_OUTPUT_PATH, "annotations", "annotations.csv"]
+    input_file = os.path.join('', *threads_sample_path)
+    df_annotations = pd.read_csv(input_file)
+    df_annotations_with_synonyms = df_annotations.copy()
+
+    keywords = df_annotations["keywords"].tolist()
+    synonym_annotations = []
+
+    for keyword in keywords:
+        for syn in wordnet.synsets(keyword):
+            for lemma in syn.lemmas():
+                synonym_annotations.append(lemma.name())
+
+    # Merge the two annotations lists
+    improved_annotations = keywords + synonym_annotations
+    
+    # Remove duplicates
+    improved_annotations = list(dict.fromkeys(improved_annotations))
+    
+    temp = "business_organization"
+    print("_" in temp)
+    # Remove keywords with "_"
+    
+    for annotation in improved_annotations:
+        if "_" in annotation or "-" in annotation:
+            print(annotation)
+            improved_annotations.remove(annotation)
+
+    # Add keywords to DataFrame
+    df_annotations_with_synonyms = pd.DataFrame()
+    df_annotations_with_synonyms["keywords"] = improved_annotations
+
+    # Write output file
+    posts_path = [PROJECT_PATH, DATA_OUTPUT_PATH, "annotations", "annotations_with_synonyms.csv"]
+    output_file = os.path.join('', *posts_path)
+    df_annotations_with_synonyms.to_csv(output_file, sep=',', encoding='utf-8')     
